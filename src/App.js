175677c6c504d0ca2d71aa8ff5428434a036c040
Ivan Popovic <ivanivopop@gmail.com>
2022-03-22 09:17:29 +0100

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Projects } from './features/projects/components/Projects';
+import React from 'react'
+import './App.css'
+import { Projects } from './features/projects/components/Projects'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        header
-      </header>
-      <Projects />
-    </div>
-  );
+   return (
+      <div className='App'>
+         <header className='header'>header</header>
+         <Routes>
+            <Route path='/' element={<Projects />} />
+         </Routes>
+      </div>
+   )
 }
 
-export default App;
+export default App
