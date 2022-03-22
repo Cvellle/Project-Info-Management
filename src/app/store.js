8eff474f6f7e6de5ee19eb60a0157d3f113ca44e
Nikola Cvetic <cuekisa@yahoo.com>
2022-03-22 11:13:17 +0100

    redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
 import projectsReducer from '../features/projects/projectsSlice';
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['projectsReducer'],
+};
+
+const rootReducer = combineReducers({
+  projects: projectsReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    projects: projectsReducer,
-  },
+  reducer: persistedReducer,
 });
