a612a0f0ee93f0de9b58ac64da1ec2ca82c289e1
Ivan Popovic <ivanivopop@gmail.com>
2022-03-31 19:42:36 +0200

    page description added

---
+++ src/features/Project/components/CreateProject.js
@@ -0,0 +1,6 @@
+import { PageDescription } from 'components/PageDescription'
+import rocket from '../../../assets/rocket.png'
+
+export const CreateProject = () => {
+  return <PageDescription title="Create Project" text="Create a new project" image={rocket} />
+}
--- src/features/Project/components/Project.js
+++ src/features/Project/components/Project.js
@@ -1,9 +1,11 @@
-import { Flex } from '@chakra-ui/layout'
+import { Flex, IconButton, Text } from '@chakra-ui/react'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
-
+import { PageDescription } from 'components/PageDescription'
 import { getProjectAssync } from '../projectSlice'
+import rocket from '../../../assets/rocket.png'
+import { MdOpenInNew } from 'react-icons/md'
 
 export function Project() {
   const dispatch = useDispatch()
@@ -14,8 +16,24 @@ export function Project() {
   }, [])
 
   return (
-    <Flex flexWrap="wrap" w="calc(1222px + 15px)" m="31px auto">
-      s
-    </Flex>
+    <>
+      <PageDescription
+        title={
+          <Flex gap="1rem">
+            {`Test Projekat`}
+            <Flex alignItems="center">
+              <IconButton icon={<MdOpenInNew />} fontSize="md" bgColor="transparent" size="xs" />
+              <Text color="gray.600" fontSize="sm">
+                EDIT
+              </Text>
+            </Flex>
+          </Flex>
+        }
+        text="Test Projekat Opis"
+        image={rocket}></PageDescription>
+      <Flex flexWrap="wrap" m="31px auto">
+        s
+      </Flex>
+    </>
   )
 }
