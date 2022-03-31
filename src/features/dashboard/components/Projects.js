43dd0cc15db9fef705e964172a15b6b1f348b0e4
Nikola Cvetic <cuekisa@yahoo.com>
2022-03-31 15:21:11 +0200

    projects component

--- src/features/dashboard/components/Projects.js
+++ src/features/dashboard/components/Projects.js
@@ -1,23 +1,42 @@
 import { Flex } from '@chakra-ui/layout'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { fetchItems, selectProjects } from '../dashboardSlice'
 import { ProjectItem } from './ProjectItem'
+import { authState } from 'features/auth/authSlice'
 
 export function Projects() {
+  const [filteredProjects, setFilteredProjects] = useState()
   const dispatch = useDispatch()
   const projectsSelector = useSelector(selectProjects)
+  const authSelector = useSelector(authState)
 
   useEffect(() => {
     dispatch(fetchItems())
+    filterProjectsFunction()
   }, [])
+
+  useEffect(() => {
+    filterProjectsFunction()
+  }, [projectsSelector])
+
+  const filterProjectsFunction = () => {
+    let finalFilteredValue = projectsSelector.filter((project) => {
+      let projectEmails = project.attributes.employees.data.map(
+        (employee) => employee.attributes.email
+      )
+      return projectEmails.includes(authSelector.currentUser.email)
+    })
+    setFilteredProjects(finalFilteredValue)
+  }
 
   return (
     <Flex flexWrap="wrap" w="calc(1222px + 15px)" m="31px auto">
-      {projectsSelector.map((project, i) => {
-        return <ProjectItem key={i} item={project} />
-      })}
+      {filteredProjects &&
+        filteredProjects.map((project, i) => {
+          return <ProjectItem key={i} item={project} />
+        })}
     </Flex>
   )
 }
