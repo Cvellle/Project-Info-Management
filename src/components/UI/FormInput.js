04c207a850b4566dec9a15f64d69b38750bf49d2
Ivan Popovic <ivanivopop@gmail.com>
2022-04-08 14:15:51 +0200

    update project

--- src/components/UI/FormInput.js
+++ src/components/UI/FormInput.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react'
+import { FormLabel, Input } from '@chakra-ui/react'
 
-const FormInput = ({ name, label, placeholder, autoComplete, register, errors }) => {
+const FormInput = ({ name, label, placeholder, autoComplete, register, defaultValue }) => {
   return (
-    <FormControl isInvalid={errors.name} isRequired>
+    <>
       {label && <FormLabel htmlFor="name">{label}</FormLabel>}
       <Input
         bgColor="white"
         id={name}
         placeholder={placeholder}
         autoComplete={autoComplete}
+        defaultValue={defaultValue}
         {...register(name, {
           required: 'This is required'
         })}
       />
-      <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
-    </FormControl>
+    </>
   )
 }
 
