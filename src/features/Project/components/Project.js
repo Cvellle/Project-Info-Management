43cc87641fd2cf154ee7ebc91ff68eea3bebd2f7
Nikola Cvetic <cuekisa@yahoo.com>
2022-04-08 16:02:06 +0200

    add note 2

--- src/features/Project/components/Project.js
+++ src/features/Project/components/Project.js
@@ -1,107 +1,20 @@
-import {
-  Flex,
-  IconButton,
-  Text,
-  Tabs,
-  Tab,
-  TabList,
-  TabPanels,
-  TabPanel,
-  Heading,
-  Avatar,
-  AvatarGroup
-} from '@chakra-ui/react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
 import { PageDescription } from 'components/PageDescription'
-import { getProjectAsync } from '../projectSlice'
-import { MdOpenInNew } from 'react-icons/md'
-import { selectedProject } from '../projectSlice'
+import rocket from '../../../assets/rocket.png'
+import ProjectForm from './ProjectForm'
+import { Box } from '@chakra-ui/react'
 
-export function Project() {
-  const dispatch = useDispatch()
-  const params = useParams()
-  const project = useSelector(selectedProject)
-
-  useEffect(() => {
-    dispatch(getProjectAsync(params.id))
-  }, [])
-
-  const url = process.env.REACT_APP_BACKEND_URL
-
+export const CreateProject = () => {
   return (
     <>
-      <PageDescription
-        title={
-          <Flex gap="1rem">
-            {project?.attributes?.name}
-            <Flex alignItems="center">
-              <IconButton icon={<MdOpenInNew />} fontSize="md" bgColor="transparent" size="xs" />
-              <Text color="gray.600" fontSize="sm">
-                EDIT
-              </Text>
-            </Flex>
-          </Flex>
-        }
-        text={project?.attributes?.description}
-        image={`${url}${project?.attributes.logo.data.attributes.url}`}>
-        <Flex gap={{ base: '1rem' }} justifyContent={{ base: 'space-around' }}>
-          <Flex flexDirection="column" gap="0.4rem">
-            <Heading as="h4" fontSize={['sm', 'lg']}>
-              Project Manager
-            </Heading>
-            <Avatar
-              size="sm"
-              src={`${url}${project?.attributes?.project_manager?.data?.attributes?.userPhoto?.data?.attributes?.url}`}
-            />
-          </Flex>
-          <Flex flexDirection="column" gap="0.24em">
-            <Heading as="h4" fontSize={['sm', 'lg']}>
-              Employees
-            </Heading>
-            <AvatarGroup size="sm" max={3}>
-              {project?.attributes.employees.data.map((employee) => (
-                <Avatar
-                  key={employee.id}
-                  name={employee.attributes.username}
-                  src={`${url}${employee.attributes.userPhoto?.data?.attributes.url}`}
-                />
-              ))}
-            </AvatarGroup>
-          </Flex>
-        </Flex>
-      </PageDescription>
-      <Tabs margin={{ base: '0', md: '2rem auto' }} maxW="1280px">
-        <TabList bgColor="#EAEAEA" color="#8E8E8E">
-          <Tab
-            _selected={{ bgColor: '#DDDDDD', color: 'black' }}
-            padding={{ base: '1rem 0.8rem', md: '1rem 1.5rem' }}>
-            <Heading as="h4" fontSize={['sm', 'lg', 'xl']}>
-              Project Management
-            </Heading>
-          </Tab>
-          <Tab
-            _selected={{ bgColor: '#DDDDDD', color: 'black' }}
-            padding={{ base: '1rem 0.8rem', md: '1rem 1.5rem' }}>
-            <Heading as="h4" fontSize={['sm', 'lg', 'xl']}>
-              Development
-            </Heading>
-          </Tab>
-          <Tab
-            _selected={{ bgColor: '#DDDDDD', color: 'black' }}
-            padding={{ base: '1rem 0.8rem', md: '1rem 1.5rem' }}>
-            <Heading as="h4" fontSize={['sm', 'lg', 'xl']}>
-              DevOps
-            </Heading>
-          </Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>Project Management</TabPanel>
-          <TabPanel>Development</TabPanel>
-          <TabPanel>DevOps</TabPanel>
-        </TabPanels>
-      </Tabs>
+      <PageDescription title="Create Project" text="Create a new project" image={rocket} />
+      <Box
+        bgColor="#F8F8F8"
+        maxW="1280px"
+        m={{ base: 'auto', md: '2rem auto' }}
+        padding={{ base: '2rem 0.6rem', md: '3rem' }}
+        borderRadius="0.4rem">
+        <ProjectForm status="create" />
+      </Box>
     </>
   )
 }
